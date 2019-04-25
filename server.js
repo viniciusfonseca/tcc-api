60511e5b9fa6fca6806a0c29ee5bc2918885d038
Vinicius Fonseca <vfonseca1618@gmail.com>
2019-04-24 23:33:10 -0300

    wip

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ const Translation = db.define('translation', {
     translation: Sequelize.STRING
 })
 
+const TranslationContext = db.define('translation', {
+    context: Sequelize.STRING
+})
+
+TranslationContext.hasOne(Translation)
+
 const User = db.define('user', {
     id: { type: Sequelize.STRING, primaryKey: true },
     email: Sequelize.STRING,
@@ -26,10 +32,17 @@ const User = db.define('user', {
 })
 
 const Exercise = db.define('exercise', {
-    translation_id_array: Sequelize.STRING,
     points: Sequelize.INTEGER,
     meta: Sequelize.STRING
 })
+
+Exercise.hasMany(Translation)
+
+const Test = db.define('test', {
+
+})
+
+Test.hasMany(Exercise)
 
 const app = express()
 
@@ -42,64 +55,64 @@ const ExerciseTypes = {
 }
 
 const createFillInTheBlank = async user => {
-    
+
 }
 
 const createAssociation = async user => {
-    
+
 }
 
 const createImageAssociation = async user => {
-    
+
 }
 
 const WORD_REGEX = /^\w+$/
 
 app.get('/translate', async (req, res) => {
-    
+
     const phrase = (req.query.t || "").trim()
     const phrase_context = req.query.ctx
-    
+
     const { text, didYouMean } = await translate(phrase, { from: 'en', to: 'pt' })
-    
+
     res.set("Content-Type", "application/json")
     res.json({ translated: text })
-    
+
     if (didYouMean) { return }
     if (!WORD_REGEX.exec(phrase)) { return }
     if (phrase === text) { return }
-    
+
     const user_id = req.query.uid
-    
+
     if (!user_id) { return }
 
     const w_count = await Translation.count({ where: { translation: text } })
-    
+
     if (w_count === 1) { return }
-    
+
     await Translation.create({
         user_id,
         translated: phrase,
         translation: text
     })
-    
+
     console.log(`Recording new transation: "${phrase}" --> "${text}"`)
-    
+
     const t_count = await Translation.count({
         where: { user_id }
     })
-    
+
     const [ user ] = await User.findCreateFind({ where: { id: user_id } })
-    
+
     if (t_count % 10 === 0 && t_count !== user.t_count) {
         const ex_type = Math.floor(Math.random() * Object.keys(ExerciseTypes).length)
-        
+
         const translations = await Translation.findAll({
             limit: 10,
             where: { user_id },
             order: [[ 'createdAt', 'DESC' ]]
         })
-        
+
         switch (ex_type) {
             case ExerciseTypes.FILL_BLANK:
                 createFillInTheBlank(translations)
@@ -112,12 +125,21 @@ app.get('/translate', async (req, res) => {
                 break
         }
     }
-    
+
     await user.update({ t_count })
 })
 
-app.post('/solve', async (req, res) => {
+app.get('/test/:id', async (req, res) => {
+
+    const id = req.params.id
+
+    const test = await Test.findOne({ where: { id } })
+
     
+})
+
+app.post('/solve', async (req, res) => {
+
 })
 
 db.sync({}).then(() =>
